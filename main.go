a0973e04aa0339138a65f7be633a249960a58309
ngirimana <chadrackngirimana@gmail.com>
2024-08-30 09:16:07 +0200

    setting up  swagger and test

--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
 
+	_ "github.com/ngirimana/AnnounceIT/docs" // Replace with your module name to match the generated docs import
+	"github.com/ngirimana/AnnounceIT/routes"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @title AnnounceIT API
+// @version 1.0
+// @description AnnounceIT is a solution for broadcasting agencies to receive and manage announcements effectively.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8000
+// @BasePath /
+
+// @schemes http https
 func main() {
 	server := gin.Default()
-	server.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
-	})
-	server.Run(":8080")
+
+	// Swagger endpoint to serve the API documentation
+	server.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	routes.RegisterRoutes(server)
+
+	// Start the server on port 8000
+	server.Run(":8000")
 }
